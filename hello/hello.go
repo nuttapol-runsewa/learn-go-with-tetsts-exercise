1abab24bbbf7cb5abaef2f672dd459e15a5ace28
Nuttapol Runsewa <nrunsewa@gmail.com>
2025-03-28 20:56:14 +0700

    feat: hellp language

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix  = "Hello, "
+	spanishhHelloPrefix = "Hola, "
+	frenchHelloPrefix   = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language, name)
+}
+
+func greetingPrefix(language, name string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishhHelloPrefix + name
+	case french:
+		prefix = frenchHelloPrefix + name
+	default:
+		prefix = englishHelloPrefix + name
+	}
+	return
 }
